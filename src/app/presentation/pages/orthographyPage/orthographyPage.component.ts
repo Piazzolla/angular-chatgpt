61149a9701e9ca9cba1e7b22f9b965ae454fd942
Mariano <macacozzz@hotmail.com>
2024-01-31 11:22:14 -0300

    conecto el prompt con el endpoint del backend

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -21,19 +21,29 @@ import { OpenAiService } from 'app/presentation/services/openai.service';
 })
 export default class OrthographyPageComponent {
 
-  public messages = signal<Message[]>([{ text: 'Hola Mundo', isGpt: false}]);
+  public messages = signal<Message[]>([]);
   public isLoading = signal(false);
   public openAiService = inject( OpenAiService );
 
   handleMessage( prompt: string ) {
-    console.log({ prompt })
-  }
+    this.isLoading.set(true);
 
-  handleMessageWithFile( {prompt, file}: TextMessageEvent ) {
-    console.log({ prompt, file })
-  }
+    this.messages.update((prev => [...prev,{ isGpt: false, text: prompt}]))
 
-  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    this.openAiService.checkOrthography( prompt ).subscribe( resp => {
+      this.isLoading.set(false);
+
+      this.messages.update( prev => [
+        ...prev,
+        {
+          isGpt: true,
+          text: resp.message,
+          info: resp
+        }
+      ])
+    });
+
+
 
   }
 
--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,5 +1,10 @@
  import { Injectable } from '@angular/core';
+import { orthographyUseCAse } from 'app/core';
+import { from } from 'rxjs';
 
  @Injectable({providedIn: 'root'})
   export class OpenAiService {
+    checkOrthography( prompt:string ) {
+      return from(orthographyUseCAse(prompt)) ;
+    }
  }
