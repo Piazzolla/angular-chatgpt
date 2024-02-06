b4fea1eb1fca18580248e4e9d2b77184a56c6e02
Mariano <macacozzz@hotmail.com>
2024-02-06 11:07:35 -0300

    use case de audio to text

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -3,3 +3,4 @@ export * from './use-cases/translate/translate.use-case';
 export * from './use-cases/pros-cons/pros-cons.use-case';
 export * from './use-cases/pros-cons/pros-cons-stream.use-case';
 export * from './use-cases/audios/text-to-audio.use-case';
+export * from './use-cases/audios/audio-to-text.use-case';
