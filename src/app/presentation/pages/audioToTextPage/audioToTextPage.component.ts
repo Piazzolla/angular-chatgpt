3cd6f0660684c9987513b81c61488c6fb21130a1
Mariano <macacozzz@hotmail.com>
2024-02-06 10:59:40 -0300

    Comienzo feature audio a texto

--- src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
+++ src/app/presentation/pages/audioToTextPage/audioToTextPage.component.ts
@@ -1,13 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, TextMessageBoxSelectComponent, TextMessageEvent, TextMessageBoxComponent, TextMessageBoxFileComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-audio-to-text-page',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxFileComponent
   ],
   templateUrl: './audioToTextPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AudioToTextPageComponent { }
+export default class AudioToTextPageComponent {
+
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject( OpenAiService );
+
+  handleMessageWithFile( {prompt, file }: TextMessageEvent ) {
+    console.log({ prompt })
+  }
+
+ }
