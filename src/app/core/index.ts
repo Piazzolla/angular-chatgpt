5af5d52888fa56d29ce3fe18ca8dc91891083c7a
Mariano <macacozzz@hotmail.com>
2024-02-08 14:47:20 -0300

    empiezo la feature de img variation a partir de img generation

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -5,3 +5,4 @@ export * from './use-cases/pros-cons/pros-cons-stream.use-case';
 export * from './use-cases/audios/text-to-audio.use-case';
 export * from './use-cases/audios/audio-to-text.use-case';
 export * from './use-cases/images/image-generation.use-case';
+export * from './use-cases/images/image-variation.use-case';
