d49928045301c04e6881e9795da35608ea58e268
Mariano <macacozzz@hotmail.com>
2024-02-08 22:34:05 -0300

    dibujar y recortar el rectangulo en la imagen

--- src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
+++ src/app/presentation/components/chat-bubbles/gptMessageEditableImage/gptMessageEditableImage.component.ts
@@ -12,15 +12,17 @@ import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, EventEmi
 })
 export class GptMessageEditableImageComponent implements AfterViewInit {
   @Input({ required: true }) text!: string;
-  @Input({ required: true }) imageInfo!: {url:string, alt:string}
+  @Input({ required: true }) imageInfo!: { url: string, alt: string }
   @ViewChild('canvas') canvasElement?: ElementRef<HTMLCanvasElement>
   @Output() onSelectedImage = new EventEmitter<string>();
 
   public originalImage = signal<HTMLImageElement | null>(null);
+  public isDrawing = signal(false);
+  public coords = signal({ x: 0, y: 0 });
 
   ngAfterViewInit(): void {
 
-    if( !this.canvasElement?.nativeElement) return;
+    if (!this.canvasElement?.nativeElement) return;
 
     const canvas = this.canvasElement.nativeElement;
     const ctx = canvas.getContext('2d');
@@ -31,18 +33,63 @@ export class GptMessageEditableImageComponent implements AfterViewInit {
 
     this.originalImage.set(image);
 
+
     image.onload = () => {
-      ctx?.drawImage( image, 0, 0, canvas.width, canvas.height);
+      ctx?.drawImage(image, 0, 0, canvas.width, canvas.height);
     }
 
 
 
   }
 
+  onMouseDown(event: MouseEvent) {
+    if (!this.canvasElement?.nativeElement) return;
 
+    this.isDrawing.set(true);
+    // Obtener las coordenadas del mouse relativo al canvas
+
+    const startX = event.clientX - this.canvasElement.nativeElement.getBoundingClientRect().left;
+    const startY = event.clientY - this.canvasElement.nativeElement.getBoundingClientRect().top;
+
+    this.coords.set({ x: startX, y: startY });
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.isDrawing()) return;
+    if (!this.canvasElement?.nativeElement) return;
+
+    const canvasRef = this.canvasElement.nativeElement;
+
+    const currentX = event.clientX - canvasRef.getBoundingClientRect().left;
+    const currentY = event.clientY - canvasRef.getBoundingClientRect().top;
+
+    //Calcular el alto y ancho de un rectangulo
+
+    const width = currentX - this.coords().x;
+    const height = currentY - this.coords().y;
+
+    // Limpiar el canvas
+    const ctx = canvasRef.getContext('2d')!;
+    ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+
+    // Redibujar la imagen
+    ctx.drawImage(this.originalImage()!, 0, 0, canvasRef.width, canvasRef.height);
+
+   // ctx.fillRect(this.coords().x, this.coords().y, width, height);
+    ctx.clearRect(this.coords().x, this.coords().y, width, height);
+  }
+
+  onMouseUp() {
+    this.isDrawing.set(false);
+
+    const canvas = this.canvasElement!.nativeElement;
+    const url = canvas.toDataURL('image/png'); //imagen en base64
+
+    this.onSelectedImage.emit(url);
+  }
 
   handleClick() {
-    this.onSelectedImage.emit( this.imageInfo.url);
+    this.onSelectedImage.emit(this.imageInfo.url);
   }
 
 
